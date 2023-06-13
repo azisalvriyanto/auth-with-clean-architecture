4b27348b0575e0f5537a6a7af4199a1aa5725bc1
Alvriyanto Azis <aalvriyanto@gmail.com>
2023-06-13 09:10:47 +0700

    test: add show all customers func of use case test

---
+++ test/user/use-case_test.go
@@ -0,0 +1,74 @@
+package user
+
+import (
+	"auth-with-clean-architecture/internal/user"
+	"auth-with-clean-architecture/internal/user/mocks"
+	"errors"
+	"reflect"
+	"testing"
+)
+
+var (
+	errorCase = errors.New("some error")
+	existData = user.User{
+		FullName: "John Doe",
+		Username: "johndoe",
+		Password: "password",
+		RoleID:   1,
+	}
+	// nilData = user.User{}
+)
+
+func TestUseCaseShowAll(t *testing.T) {
+	type fields struct {
+		R user.RepositoryInterface
+	}
+
+	successCase := []user.User{}
+	for i := 0; i < 5; i++ {
+		successCase = append(successCase, existData)
+	}
+
+	mockRepository := mocks.NewRepositoryInterface(t)
+	mockRepository.EXPECT().ShowAll().Return(successCase, nil).Once()
+	mockRepository.EXPECT().ShowAll().Return(nil, errorCase).Once()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []user.User
+		wantErr bool
+	}{
+		{
+			name: "success case",
+			fields: fields{
+				R: mockRepository,
+			},
+			want:    successCase,
+			wantErr: false,
+		},
+		{
+			name: "error case",
+			fields: fields{
+				R: mockRepository,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &user.UseCase{
+				R: tt.fields.R,
+			}
+			got, err := u.ShowAll()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UseCase.ShowAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UseCase.ShowAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
